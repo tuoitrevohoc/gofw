184b48eac4d154724d3ef4a4815c1a95b6454de0
Tran Thien Khiem <tuoitrevohoc@me.com>
2025-03-12 15:57:39 +0000

    Add action buttons

--- frontend/src/routes/restaurants/RestaurantList.tsx
+++ frontend/src/routes/restaurants/RestaurantList.tsx
@@ -1,4 +1,4 @@
-import { Add } from "@mui/icons-material";
+import { Add, Delete, Edit } from "@mui/icons-material";
 import {
   Button,
   Stack,
@@ -48,16 +48,57 @@ export default function RestaurantList() {
         <Card>
           <CardContent>Your restaurants</CardContent>
           <DataGrid
-            checkboxSelection
             rows={restaurants.edges!.map((edge) => edge!.node!)}
             columns={[
               { field: "name", headerName: "Name", flex: 1 },
-              { field: "address", headerName: "Address", flex: 1 },
+              {
+                field: "address",
+                headerName: "Address",
+                flex: 1,
+                sortable: false,
+              },
+              {
+                field: "actions",
+                headerName: "Actions",
+                sortable: false,
+                width: 200,
+                filterable: false,
+                hideable: false,
+                renderCell: () => {
+                  return (
+                    <Stack
+                      direction="row"
+                      gap={2}
+                      alignItems="center"
+                      justifyContent="center"
+                      height="100%"
+                    >
+                      <Button size="small" variant="text" startIcon={<Edit />}>
+                        Edit
+                      </Button>
+                      <Button
+                        size="small"
+                        variant="text"
+                        color="warning"
+                        startIcon={<Delete />}
+                      >
+                        Delete
+                      </Button>
+                    </Stack>
+                  );
+                },
+              },
             ]}
             sx={{
               border: "none",
               "& .MuiDataGrid-columnHeaders": {
                 backgroundColor: "lightgray",
+              },
+              "& .MuiDataGrid-iconSeparator": {
+                color: "transparent",
+              },
+              "& .MuiDataGrid-withBorderColor": {
+                border: "none",
               },
             }}
             pageSizeOptions={[5, 10, 25]}
